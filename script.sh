4c054d8c94f4e12ab8d04c70c266df5e29e9bc21
Hovo Petrosyan <root@localhost.localdomain>
2018-05-03 06:33:49 -0400

    sd

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@
 path_to="/home/$USER/logs"
 
 echo $USER  
+mkdir -p $path_to
 curl karmir.am > "$path_to/karmir"
 curl kapuyt.am > "$path_to/kapuyt"
 diff "$path_to/karmir" "$path_to/kapuyt" >> "$path_to/log"
